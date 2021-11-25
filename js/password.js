0c855d8b007ac3a022796a0342d861e073e0ad89
midnight4577 <611532@meischools.org>
2021-11-25 10:57:45 -0800

    Added a "password" example

---
+++ js/password.js
@@ -0,0 +1,40 @@
+// JavaScript Document
+var nums = [];
+const correctNums = [1, 2, 3, 4];//this would need to be encrypted
+
+function arraysEqual(a, b) {
+  if (a === b) return true;
+  if (a == null || b == null) return false;
+  if (a.length !== b.length) return false;
+
+  // If you don't care about the order of the elements inside
+  // the array, you should sort both arrays here.
+  // Please note that calling sort on an array will modify that array.
+  // you might want to clone your array first.
+
+  for (var i = 0; i < a.length; ++i) {
+    if (a[i] !== b[i]) return false;
+  }
+  return true;
+}
+
+function addNum(num){
+	nums.push(num);
+	return nums;
+}
+function clearNums(){
+	nums.length = 0;
+	return nums;
+}
+function checkNums(){
+	//would have to encrypt "nums" then check wether the 2 encrypted variables
+	//this would still be increadably easy to brute force
+	if (arraysEqual(nums, correctNums)){
+		return "Correct";
+	}
+	else{
+		nums.length = 0;
+		return "Incorrect";
+	}
+}
+
