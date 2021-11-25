8fe550a88176c8bffa05150a0f196d13ec0e3099
Andrew <45957548+midnight4577@users.noreply.github.com>
2021-11-24 17:30:01 -0800

    Updated functions

--- js/test.js
+++ js/test.js
@@ -1,26 +1,6 @@
 // JavaScript Document
 var nums = [];
-function one(){
-	nums.push("1");
-	return nums;
-}
-function two(){
-	nums.push("2");
-	return nums;
-}
-function three(){
-	nums.push("3");
-	return nums;
-}
-function four(){
-	nums.push("4");
-	return nums;
-}
-function five(){
-	nums.push("5");
-	return nums;
-}
-function six(){
-	nums.push("6");
+function add(var num){
+	nums.push(num);
 	return nums;
 }
